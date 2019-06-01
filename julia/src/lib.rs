34fd0ad91d75509d226122c4f65b88173844daaf
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-06-01 09:45:18 +0900

    Split to util module

--- julia/src/lib.rs
+++ julia/src/lib.rs
@@ -1,19 +1,6 @@
 //! Safe wrapper for Julia C API
 
-pub use julia_sys::*;
+mod util;
+pub use util::*;
 
-pub unsafe fn jl_astaggedvalue(ptr: *mut jl_value_t) -> *mut jl_taggedvalue_t {
-    let p = ptr as *mut jl_taggedvalue_t;
-    p.offset(-1)
-}
-
-pub unsafe fn jl_typeof(ptr: *mut jl_value_t) -> *mut jl_datatype_t {
-    let tagged = jl_astaggedvalue(ptr);
-    let t = (*tagged).__bindgen_anon_1.header & !15_usize;
-    t as *mut jl_datatype_t
-}
-
-pub unsafe fn jl_is_array(ptr: *mut jl_value_t) -> bool {
-    let ty = jl_typeof(ptr);
-    (*ty).name == jl_array_typename
-}
+pub mod module;
